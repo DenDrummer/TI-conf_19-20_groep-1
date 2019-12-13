0c42865a7057dbe77f4eec46139591adf7ee6272
NathanKdG <48127258+NathanKdG@users.noreply.github.com>
2019-12-13 13:31:03 +0100

    Added Rotation Edit

--- POC/MoveRequestHandler.cs
+++ POC/MoveRequestHandler.cs
@@ -10,6 +10,14 @@ public class MoveRequestHandler : MonoBehaviour
     [Require] private MoveCommandSender _commandSender;
 
     const int RaycastRange = 100;
+    const float ScaleModifier = 1f;
+    const float RotationModifier = 1f;
+    const Vector3 InitOffset = new Vector3(7.0E-11f,0,0);
+
+    private Vector3 FingerDistance;
+    private Vector3 FingerDistanceDelta = new Vector3(0,0,0);
+    private Vector3 FingerAngle;
+    private Vector3 FingerAngleDelta = new Vector3(0,0,0);
 
     private GameObject shape;
 
@@ -29,10 +37,43 @@ public class MoveRequestHandler : MonoBehaviour
                 shape = hit.GameObject
             }
         }
+
         else if(touchCount == 1)
         {
             shape.transform.position += currentTouch.deltaPosition;
             _commandSender.SendMoveCommand(_entityId, new MoveRequest());
+        }
+
+        else if(touchCount == 2)
+        {
+            Vector3 LastFingerDistance = FingerDistance;
+            Vector3 LastFingerAngle = FingerAngle;
+
+            Vector3 finger1Pos = Input.GetTouch(0).position;
+            Vector3 finger2Pos = Input.GetTouch(1).position;
+            
+            FingerDistance = finger1Pos - finger2Pos;
+            if(FingerDistanceDelta == new Vector3(0,0,0))
+            {
+                FingerDistanceDelta = InitOffset;
+            }
+            else
+            {
+                FingerDistanceDelta = LastFingerDistance - FingerDistance;
+            }
+
+            FingerAngle = Vector3.Angle(finger1Pos, finger2Pos);
+            if(FingerAngleDelta == new Vector3(0,0,0))
+            {
+                FingerAngleDelta == InitOffset;
+            }
+            else
+            {
+                FingerAngleDelta = FingerAngle - LastFingerAngle;
+            }
+
+            shape.transform.scale = FingerDistanceDelta * ScaleModifier;
+            shape.transform.rotation = FingerAngleDelta * RotationModifier;
         }
     }
 }
