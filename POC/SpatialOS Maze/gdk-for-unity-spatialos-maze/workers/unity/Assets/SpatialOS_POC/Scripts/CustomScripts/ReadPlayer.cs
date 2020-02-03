0cfcf03d92d3256f5a4e09e814e4e83e05e786b2
DenDrummer <muzikant96@gmail.com>
2020-02-03 23:15:55 +0100

    deployment + adding player schema
    deployment working
    player schema file created, still need to be implemented
    walls added to side of the playing field to prevent falling into the void

---
+++ POC/SpatialOS Maze/gdk-for-unity-spatialos-maze/workers/unity/Assets/SpatialOS_POC/Scripts/CustomScripts/ReadPlayer.cs
@@ -0,0 +1,15 @@
+﻿using Be.Kdg.SpatialosMaze;
+using Improbable.Gdk.Subscriptions;
+using UnityEngine;
+
+public class ReadPlayer : MonoBehaviour
+{
+    [Require]
+    private PlayerReader _reader;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // TODO: get nameplate and update player name
+    }
+}
---
+++ POC/SpatialOS Maze/gdk-for-unity-spatialos-maze/workers/unity/Assets/SpatialOS_POC/Scripts/CustomScripts/WritePlayer.cs
@@ -0,0 +1,20 @@
+﻿using Be.Kdg.SpatialosMaze;
+using Improbable;
+using Improbable.Gdk.Subscriptions;
+using UnityEngine;
+
+public class WritePlayer : MonoBehaviour
+{
+    [Require]
+    private PlayerWriter _writer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Player.Update update = new Player.Update {
+            // TODO: generate random name/id or ask name (asking brings risk of trolls during demo) ~Jorden
+            Name = "Guest"          
+        };
+        _writer.SendUpdate(update);
+    }
+}
