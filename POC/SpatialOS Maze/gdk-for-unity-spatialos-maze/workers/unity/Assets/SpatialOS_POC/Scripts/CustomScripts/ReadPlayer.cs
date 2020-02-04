ec925f9a8442504c87b0d3c7b23a49ebe91ba21d
DenDrummer <muzikant96@gmail.com>
2020-02-04 19:59:19 +0100

    v0.0.4

--- POC/SpatialOS Maze/gdk-for-unity-spatialos-maze/workers/unity/Assets/SpatialOS_POC/Scripts/CustomScripts/ReadPlayer.cs
+++ POC/SpatialOS Maze/gdk-for-unity-spatialos-maze/workers/unity/Assets/SpatialOS_POC/Scripts/CustomScripts/ReadPlayer.cs
@@ -1,15 +1,19 @@
-﻿using Be.Kdg.SpatialosMaze;
+﻿using Be.Kdg.SpatialosMaze.Player;
 using Improbable.Gdk.Subscriptions;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ReadPlayer : MonoBehaviour
 {
     [Require]
     private PlayerReader _reader;
 
+    [SerializeField]
+    private Text namePlate;
+
     // Update is called once per frame
     void Update()
     {
-        // TODO: get nameplate and update player name
+        namePlate.text = _reader.Data.Name;
     }
 }
